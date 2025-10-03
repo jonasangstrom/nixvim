fd9408cc5a7fe8c76ebdc332a53f742c8cac4b6a
Jonas Ångström <jonas.angstrom@scientaomicron.com>
2025-10-03 15:43:08 +0200

    Re-formated file and added docker as lsp.

--- config/lsps.nix
+++ config/lsps.nix
@@ -1,35 +1,36 @@
 {
     plugins = {
-	lsp-format = {
-	    enable = true;
-	    lspServersToEnable = "all";
-	};
-	lsp = {
-	    enable = true;
-	    inlayHints = true;
-	    keymaps = {
-		diagnostic = {
-		    "<leader>n" = "goto_next";
-		    "<leader>N" = "goto_prev";
+		lsp-format = {
+			enable = true;
+			lspServersToEnable = "all";
 		};
-		lspBuf = {
-		    "gd" = "definition";
-		    "gD" = "references";
-		    "rn" = "rename";
-		    "K" = "hover";
-		    "gi" = "implementation";
+		lsp = {
+			enable = true;
+			inlayHints = true;
+			keymaps = {
+				diagnostic = {
+					"<leader>n" = "goto_next";
+					"<leader>N" = "goto_prev";
+				};
+				lspBuf = {
+					"gd" = "definition";
+					"gD" = "references";
+					"rn" = "rename";
+					"K" = "hover";
+					"gi" = "implementation";
+				};
+			};
+			servers = {
+				nixd = {
+					enable = true;
+					settings = {
+					expr = "import <nixpkgs> {}";
+					};
+				};
+				lua_ls.enable = true;
+				pyright.enable = true;
+				dockerls.enable = true;
+			};
 		};
-	    };
-	    servers = {
-		nixd = {
-		    enable = true;
-		    settings = {
-			expr = "import <nixpkgs> {}";
-		    };
-		};
-		lua_ls.enable = true;
-		pyright.enable = true;
-            };
-	};
     };
 }
