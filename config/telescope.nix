26c79071b6439f4531ee4505be2c65fba64dec3a
Jonas Ångström <jonas.angstrom@scientaomicron.com>
2025-10-13 14:09:48 +0200

    Added find hidden files.

--- config/telescope.nix
+++ config/telescope.nix
@@ -1,11 +1,21 @@
 {
     plugins.telescope = {
-	enable = true;
-	keymaps  = 
-	{
-	    "<leader>sf" = "find_files";
-	    "<leader>sg" = "live_grep";
-	    "<leader>sb" = "buffers";
-	};
+        enable = true;
+        keymaps  = {
+            "<leader>sf" = "find_files";
+            "<leader>sg" = "live_grep";
+            "<leader>sb" = "buffers";
+        };
     };
+
+extraConfigLua = ''
+    local telescope = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>sh", function()
+    telescope.find_files({
+        hidden = true,
+        no_ignore = true,
+        file_ignore_patterns = { "%.git/" }
+    })
+    end, { desc = "Find files (including hidden and ignored)" })
+    '';
 }
