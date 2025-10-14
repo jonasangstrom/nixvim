412de0d4e8ef0bdd34d83ab6f91ef3af60712581
Jonas Ångström <jonas.angstrom@scientaomicron.com>
2025-10-14 09:04:59 +0200

    now make derivation to no pollute env

--- flake.nix
+++ flake.nix
@@ -1,65 +1,63 @@
 {
-  description = "A nixvim configuration";
+    description = "A nixvim configuration";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        nixvim.url = "github:nix-community/nixvim";
+        flake-parts.url = "github:hercules-ci/flake-parts";
+    };
 
-  outputs =
+    outputs =
     { nixvim, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+        systems = [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+        ];
 
-      perSystem =
-        { system, pkgs, ... }:
-        let
-          nixvimLib = nixvim.lib.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-          nixvimModule = {
-            inherit system; # or alternatively, set `pkgs`
-            module = import ./config { inherit pkgs; }; # import the module directly
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
-            extraSpecialArgs = {
-              # inherit (inputs) foo;
+        perSystem =
+            { system, pkgs, ... }:
+            let
+                nixvimLib = nixvim.lib.${system};
+                nixvim' = nixvim.legacyPackages.${system};
+                nixvimModule = {
+                    inherit system; # or alternatively, set `pkgs`
+                    module = import ./config { inherit pkgs; }; # import the module directly
+                    # You can use `extraSpecialArgs` to pass additional arguments to your module files
+                    extraSpecialArgs = {
+                        # inherit (inputs) foo;
+                    };
+                };
+                nvim = nixvim'.makeNixvimWithModule nixvimModule;
+            in
+            {
+                checks = {
+                    # Run `nix flake check .` to verify that your config is not broken
+                    default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+                };
+
+                packages = {
+                    # Lets you run `nix run .` to start nixvim
+                    default = pkgs.stdenv.mkDerivation {
+                        name = "nvim-wrapper";
+                        src = null;
+                        phases = [ "installPhase" ];
+                        buildInputs = [ pkgs.makeWrapper ];
+                        installPhase = ''
+                            mkdir -p $out/bin
+                            ln -s ${nvim}/bin/nvim $out/bin/nvim
+                            wrapProgram $out/bin/nvim \
+                            --prefix PATH : ${pkgs.lib.makeBinPath [
+                            pkgs.ripgrep
+                            pkgs.fd
+                            pkgs.dotnet-sdk_9
+                            pkgs.omnisharp-roslyn
+                            ]}
+                        '';
+                    };
+                };
             };
-          };
-          nvim = nixvim'.makeNixvimWithModule nixvimModule;
-        in
-        {
-          checks = {
-            # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          };
-
-          packages = {
-            # Lets you run `nix run .` to start nixvim
-			default = pkgs.symlinkJoin {
-    name = "nvim";
-    paths = [
-      nvim
-      pkgs.ripgrep
-	  pkgs.omnisharp-roslyn
-      pkgs.dotnet-sdk_9
-	  pkgs.fd
-	  pkgs.libunwind
-    ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/nvim \
-        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.ripgrep ]} \
-        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.fd ]} \
-        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.dotnet-sdk_9 ]} \
-        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.omnisharp-roslyn ]} 
-    '';
-  };
-          };
         };
-    };
 }
