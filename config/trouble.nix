77d4615041204e58c2e6a11142e529d5784407cc
Jonas Ångström <jonas.angstrom@scientaomicron.com>
2025-10-20 16:11:51 +0200

    added keymap for trouble

--- config/trouble.nix
+++ config/trouble.nix
@@ -4,4 +4,11 @@
             enable = true;
         };
 	};
+    keymaps = [
+        {
+            key =  "<leader>t";
+            action = "<cmd>Trouble diagnostics toggle<cr>";
+	        options.silent = true;
+        }
+    ];
 }
