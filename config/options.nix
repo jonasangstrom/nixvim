340c028bfc4e16bfe51f5c418266f47d6ecad29a
Jonas Ångström <jonas.angstrom@scientaomicron.com>
2026-01-19 14:30:30 +0100

    nix files now indent with 2 spaces on tab

--- config/options.nix
+++ config/options.nix
@@ -10,4 +10,14 @@
     smartcase = true;
     clipboard = "unnamedplus";
   };
+  extraFiles."after/ftplugin/nix.lua".text = ''
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+
+    -- Some Nix plugins force a complex 'indentexpr'. 
+    -- If the indentation is still 'jumping' weirdly, uncomment the line below:
+    -- vim.opt_local.indentexpr = "" 
+  '';
 }
