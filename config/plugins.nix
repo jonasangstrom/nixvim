41f79cf8e478b6fea33832ce03a5b26fb8bf06f2
Jonas Ångström <jonas.aangstroem@gmail.com>
2026-01-31 12:52:46 +0100

    Added undotree.

--- config/plugins.nix
+++ config/plugins.nix
@@ -2,10 +2,10 @@
   plugins.lualine.enable = true;
   plugins.web-devicons.enable = true;
   plugins.which-key.enable = true;
-
   plugins.gitsigns.enable = true;
   imports = [
     ./telescope.nix
+    ./undotree.nix
     ./oil.nix
     ./fugitive.nix
     ./treesitter.nix
