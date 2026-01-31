8520feff3adc911d4c4d1fe7b61ff4a4d293bdd1
Jonas Ångström <jonas.aangstroem@gmail.com>
2026-01-31 13:17:56 +0100

    Updated flake and fix consequences.

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -6,12 +6,11 @@
       settings = {
         highlight.enable = true;
         indent.enable = true;
+        refactor = {
+          enable = true;
+          settings = { highlight_definitions.enable = true; };
+        };
       };
-    };
-
-    treesitter-refactor = {
-      enable = true;
-      settings = { highlight_definitions = { enable = true; }; };
     };
 
     hmts.enable = true;
